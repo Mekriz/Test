db4c0a813fdc096c0ea0af3b50241921b204c772
MeKriz <98299729+Mekriz@users.noreply.github.com>
2022-05-19 06:18:45 +0300

    Add biutiful logo! ❤️

--- Test.php
+++ Test.php
@@ -1,28 +1,29 @@
 <?php
 $version = "1.0.1"; //really? simple var??
 function checkVersion($version){
-  $latest = file_get_contents("https://pastebin.com/raw/QsVueNFX");
-  if($latest != $version){
+  $latest = explode(" ", file_get_contents("https://pastebin.com/raw/QsVueNFX"));
+  if($latest[0] != $version){
     send("Качай новую $latest версию пжж");
     exit;
   }
+return $latest[1];
 }
 
+$logoColor = checkVersion($version);
 function send($msg){
   if($msg == ""){
-    echo "
+    echo "$logoColor
 ███╗░░░███╗███████╗██╗░░██╗██████╗░██╗███████╗
 ████╗░████║██╔════╝██║░██╔╝██╔══██╗██║╚════██║
 ██╔████╔██║█████╗░░█████═╝░██████╔╝██║░░███╔═╝
 ██║╚██╔╝██║██╔══╝░░██╔═██╗░██╔══██╗██║██╔══╝░░
 ██║░╚═╝░██║███████╗██║░╚██╗██║░░██║██║███████╗
-╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝╚══════╝";
+╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝╚══════╝$white";
     return true;
   }
   echo "\n$msg";
 }
 
-checkVersion($version);
 send("");
 while(true){
   switch("тест"){
