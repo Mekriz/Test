a8fc320f892cfb4ea5169bc80078f37ae567b3b1
MeKriz <98299729+Mekriz@users.noreply.github.com>
2022-05-19 05:51:18 +0300

    Update Test.php
    AAh SWITCH!❤️

--- Test.php
+++ Test.php
@@ -1,18 +1,20 @@
 <?php
 function send($msg){
-if($msg == ""){
-echo "\n\n   ***\n Test...\n  (c)MK\n   ***\n\n\n";
-return true;
-}
-echo "\n$msg";
+  if($msg == ""){
+    echo "\n\n   ***\n Test...\n  (c)MK\n   ***\n\n\n";
+    return true;
+  }
+  echo "\n$msg";
 }
 
 send("");
 while(true){
-if("тест" == "тест"){
-send("это действительно тест");
-}else{
-send("по скольку теста небыло, мы его провели: ". exec("uptime"));
-}
-sleep(1);
+  switch("тест"){
+    case "тест":
+    send("это действительно тест");
+    break;
+    default:
+    send("по скольку теста небыло, мы его провели: ". exec("uptime");
+  }
+  sleep(1);
 }
