b3745e6b6d950e5604cb129ebf1a05753912995d
MeKriz <98299729+Mekriz@users.noreply.github.com>
2022-05-19 06:01:35 +0300

    Ohh my!!!! Add checkVersion! ❤️

--- Test.php
+++ Test.php
@@ -1,4 +1,13 @@
 <?php
+$version = "1.0.1"; //really? simple var??
+function checkVersion($version){
+  $latest = file_get_contents("https://pastebin.com/raw/QsVueNFX");
+  if($latest != $version){
+    send("Качай новую $latest версию пжж");
+    exit;
+  }
+}
+
 function send($msg){
   if($msg == ""){
     echo "\n\n   ***\n Test...\n  (c)MK\n   ***\n\n\n";
@@ -7,6 +16,7 @@ function send($msg){
   echo "\n$msg";
 }
 
+checkVersion($version);
 send("");
 while(true){
   switch("тест"){
